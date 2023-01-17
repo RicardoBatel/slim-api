ec70f3c343922f900b59437b15627fcd986c37cd
RicardoBatel <ricardo.batel@hotmail.com>
2023-01-17 15:13:12 -0300

    Usando o Illuminate para acessar o DB

--- src/routes/produtos.php
+++ src/routes/produtos.php
@@ -2,13 +2,20 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use App\Models\Produto;
 
-// Routes
+// Rotas para produtos
+
+//ORM -> Object Relational Mapper (Mapeador de objeto relacional)
+//Iluminte -> é o motor da base de dados do Laravel sem o Lravel
+//Eloquent ORM
 $app->group('/api/v1', function(){
 
-  $this->get('/produtos', function($request, $response){
+  $this->get('/produtos/lista', function($request, $response){
 
-    return $response->withJson(['nome'=>'Moto G']);
+    $produto = Produto::get();
+
+    return $response->withJson($produto);
 
   });
 
